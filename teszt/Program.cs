a752fa8793b5ae8279d62110f27e4fd904403568
LI <LI@DESKTOP-VDOGF3H>
2021-09-20 13:55:12 +0200

    Add project files.

---
+++ teszt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace teszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
